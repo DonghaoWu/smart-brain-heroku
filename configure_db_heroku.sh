407e755d8b5ed9509fe4ab4238cc3a18d0404ec5
Donghao Wu <donghao.wu.us@gmail.com>
2021-04-24 19:07:02 -0400

    debugging.

--- configure_db_heroku.sh
+++ configure_db_heroku.sh
@@ -5,6 +5,6 @@ echo "Configuring heroku postgre database..."
 heroku pg:reset DATABASE
 
 heroku pg:psql < ./sql/account.sql
-heroku pg:psql < ./sql/accountProflie.sql
+heroku pg:psql < ./sql/accountProfile.sql
 
 echo "Heroku postgre database configured!"
