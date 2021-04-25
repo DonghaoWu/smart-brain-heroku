c4d3e9d3f118c7099cc330c3d94128b610fb83f8
Donghao Wu <donghao.wu.us@gmail.com>
2021-04-24 22:00:45 -0400

    edited ./backend/server.js code to align frontend directory name.

--- backend/server.js
+++ backend/server.js
@@ -36,9 +36,9 @@ app.use((err, req, res, next) => {
 })
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend-smart-brain-prod/build')));
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
   app.use((req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend-smart-brain-prod/build/index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
   })
 }
 
